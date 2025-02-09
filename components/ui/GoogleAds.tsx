b8f49c642eec0dcf82d299c831b3e5b3b3b0c6dc
unknown <chitkoko124706@gmail.com>
2025-02-10 05:30:30 +0630

    ads

--- components/ui/GoogleAds.tsx
+++ components/ui/GoogleAds.tsx
@@ -15,9 +15,26 @@ const GoogleAds = () => {
       script.crossOrigin = "anonymous";
       document.head.appendChild(script);
     }
+    // Manually trigger AdSense
+    try {
+      //@ts-ignore
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error("AdSense failed to load", e);
+    }
   }, []);
 
-  return null; // No need to return anything for Auto Ads
+  // return null;
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-5952419186869307"
+      data-ad-slot="4396157574"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
+  );
 };
 
 export default GoogleAds;
